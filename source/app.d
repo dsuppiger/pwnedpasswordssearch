6fc46e63c8b4f4b74833dc26f50095fcb91eb745
David Suppiger <david_suppiger@bluewin.ch>
2019-02-09 14:11:44 +0100

    Implemented command line tool to check passwords from CSV file against the pwned passwords of haveibeenpwned.com.

--- source/app.d
+++ source/app.d
@@ -1,6 +1,69 @@
+import requests;
+import core.stdc.stdlib;
+import std.conv;
+import std.csv;
+import std.digest.sha;
+import std.file;
 import std.stdio;
+import std.string;
+import std.uni;
 
-void main()
+void main(string[] args)
 {
-    writeln("Edit source/app.d to start your project.");
+    if (args.length < 2) {
+        writeln("Please provide a CSV with a column 'password' and additional columns.");
+        exit(1);
+    }
+
+    string inputFile = args[1];
+    auto data = readText(inputFile);
+
+    auto rq = new Request();
+    string pwHeader = null;
+    foreach (record; csvReader!(string[string])(data, null))
+    {
+        if (pwHeader == null)
+        {
+            foreach (header; record.keys)
+            {
+                if (header.sicmp("password") == 0)
+                {
+                    pwHeader = header;
+                    break;
+                }
+            }
+
+            if (pwHeader == null)
+            {
+                writeln("Invalid CSV. Please provide a 'password' column.");
+                exit(1);
+            }
+        }
+
+        auto pwHash = toHexString(sha1Of(record[pwHeader]));
+
+        auto rs = rq.get("https://api.pwnedpasswords.com/range/" ~ pwHash[0..5].to!string);
+
+        if (rs.code == 200)
+        {
+            string responseData = cast(string)rs.responseBody.data;
+
+            if (responseData.indexOf(pwHash[5..$], No.caseSensitive) > -1)
+            {
+                writeln("The password for the following entry was pwned!");
+                foreach (key; record.keys)
+                {
+                    if (key != pwHeader) writeln(key ~ ": " ~ record[key]);
+                }
+            }
+        }
+        else
+        {
+            writeln("Failed to check the following entry (returned status code " ~ rs.code.to!string ~ "):");
+            foreach (key; record.keys)
+            {
+                if (key != pwHeader) writeln(key ~ ": " ~ record[key]);
+            }
+        }
+    }
 }
